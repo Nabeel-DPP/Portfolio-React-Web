6a507ef2a18423c8584f353fc0f173a6ec073f32
Nabeel-DPP <nabeelhussain5359@gmail.com>
2024-07-30 10:47:36 +0500

    Add New Map Button in Navigation Bar

--- react-project/src/components/layout/Navbar.jsx
+++ react-project/src/components/layout/Navbar.jsx
@@ -17,7 +17,9 @@ function Navbar()
                     <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/">Home</Link></li>
                         <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/portfolio">Portfolio</Link></li>
                         <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/weather">Weather</Link></li>
+                        <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/map">Map</Link></li>
                         <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/about">About</Link></li>
+
                         <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/contact">Contact</Link></li>
                     </ul>
                 </div>
